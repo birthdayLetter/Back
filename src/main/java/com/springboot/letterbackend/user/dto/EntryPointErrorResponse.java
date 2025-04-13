0376a7d8b85111e0c197257158b1603828fa3a67
seonkyoung <sunkyoung.dev@gmail.com>
2025-04-13 12:46:08 +0900

    EntryPointErrorResponse 작성

---
+++ src/main/java/com/springboot/letterbackend/user/dto/EntryPointErrorResponse.java
@@ -0,0 +1,14 @@
+package com.springboot.letterbackend.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class EntryPointErrorResponse {
+    private String message;
+}
