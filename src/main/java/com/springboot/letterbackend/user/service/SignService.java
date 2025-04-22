eaf30ef046068ec1a9dc587d378035258ccefc50
seonkyoung <sunkyoung.dev@gmail.com>
2025-04-22 09:55:12 +0900

    SignService.java 생일 변수 추가

--- src/main/java/com/springboot/letterbackend/user/service/SignService.java
+++ src/main/java/com/springboot/letterbackend/user/service/SignService.java
@@ -4,8 +4,11 @@ package com.springboot.letterbackend.user.service;
 import com.springboot.letterbackend.user.dto.SignInResultDto;
 import com.springboot.letterbackend.user.dto.SignUpResultDto;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public interface SignService {
-    SignUpResultDto signUp(String id, String password, String name, String role,String url);
+    SignUpResultDto signUp(String id, String password, String name, String role, String url, LocalDateTime birthday);
     SignInResultDto signIn(String id, String password) throws  RuntimeException;
 
 
