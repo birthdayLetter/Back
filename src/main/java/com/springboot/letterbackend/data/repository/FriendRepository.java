b9e6a435d8bd97a643cbd440cdc0c33f9cd6a848
seonkyoung <sunkyoung.dev@gmail.com>
2025-04-22 09:59:43 +0900

    FriendRepository 메서드 추가

--- src/main/java/com/springboot/letterbackend/data/repository/FriendRepository.java
+++ src/main/java/com/springboot/letterbackend/data/repository/FriendRepository.java
@@ -4,4 +4,6 @@ import com.springboot.letterbackend.data.entity.Friend;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FriendRepository extends JpaRepository<Friend, Long> {
+   Friend findFriendByUserIdAndFriendId(String userId,String friendId);
+   Friend findFriendByUserId(String userId);
 }
