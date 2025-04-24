01e25e08e41417c1d5f4d2669709011c4bea03f7
seonkyoung <sunkyoung.dev@gmail.com>
2025-04-25 00:45:38 +0900

    SiginService: signUp 에 role 변수제거

--- src/main/java/com/springboot/letterbackend/user/service/SignService.java
+++ src/main/java/com/springboot/letterbackend/user/service/SignService.java
@@ -8,7 +8,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public interface SignService {
-    SignUpResultDto signUp(String id, String password, String name, String role, String url, LocalDateTime birthday);
+    SignUpResultDto signUp(String id, String password, String name, String url, LocalDateTime birthday);
     SignInResultDto signIn(String id, String password) throws  RuntimeException;
 
 
