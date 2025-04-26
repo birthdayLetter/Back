8f6936c5e6ebafac54d24e0d618165e7b6c2b3d4
seonkyoung <sunkyoung.dev@gmail.com>
2025-04-27 00:04:25 +0900

    UserProfileService: getUserProfile, editUserProfile 메서드 작성

--- src/main/java/com/springboot/letterbackend/user/service/UserProfileService.java
+++ src/main/java/com/springboot/letterbackend/user/service/UserProfileService.java
@@ -1,12 +1,14 @@
 package com.springboot.letterbackend.user.service;
 
-import com.springboot.letterbackend.user.dto.UserProfileReponseDTO;
+import com.springboot.letterbackend.data.entity.User;
+import com.springboot.letterbackend.user.dto.UserProfileResponseDTO;
+import com.springboot.letterbackend.user.dto.UserProfileRequestDTO;
 
 public interface UserProfileService {
     //유저 기본정보를 불러오는 서비스
-    UserProfileReponseDTO getUserProfile();
+    UserProfileResponseDTO getUserProfile();
 
     // 유저 개인 정보를 수정하는 서비스
-
+    UserProfileResponseDTO editUserProfile(UserProfileRequestDTO userProfileReponseDTO, User user);
     // 유저가 편지를 클릭 했을때 읽었다고 상태를 수정합니다.
 }
