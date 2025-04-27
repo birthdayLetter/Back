e0fd8dfbf5b57040fdca60ec6838cb82288915f7
seonkyoung <sunkyoung.dev@gmail.com>
2025-04-27 15:47:52 +0900

    CheckServiceImpl: cheskPassword메서드 구현

--- src/main/java/com/springboot/letterbackend/user/service/impl/CheckServiceImpl.java
+++ src/main/java/com/springboot/letterbackend/user/service/impl/CheckServiceImpl.java
@@ -3,15 +3,18 @@ package com.springboot.letterbackend.user.service.impl;
 import com.springboot.letterbackend.data.entity.User;
 import com.springboot.letterbackend.data.repository.UserRepository;
 import com.springboot.letterbackend.user.service.CheckService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CheckServiceImpl implements CheckService {
 
     final UserRepository userRepository;
+    final PasswordEncoder passwordEncoder;
 
-    public CheckServiceImpl(UserRepository userRepository) {
+    public CheckServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
 
@@ -31,6 +34,16 @@ public class CheckServiceImpl implements CheckService {
             return true;
         else
             return false;
+
+    }
+
+    @Override
+    public boolean CheckPassword(String enterPassword,User user) {
+        if(passwordEncoder.matches(enterPassword,user.getPassword())){
+            return true;
+        }
+        return false;
+
 
     }
 }
