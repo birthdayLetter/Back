b4cdfa7e1377f713005603d9d36b056ed183b3eb
seonkyoung <sunkyoung.dev@gmail.com>
2025-04-27 15:47:35 +0900

    CheckService: cheskPassword 추가 비밀번호 인증

--- src/main/java/com/springboot/letterbackend/user/service/CheckService.java
+++ src/main/java/com/springboot/letterbackend/user/service/CheckService.java
@@ -1,6 +1,9 @@
 package com.springboot.letterbackend.user.service;
 
+import com.springboot.letterbackend.data.entity.User;
+
 public interface CheckService {
     boolean CheckEmail(String email);
     boolean CheckUid(String uid);
+    boolean CheckPassword(String password, User user);
 }
