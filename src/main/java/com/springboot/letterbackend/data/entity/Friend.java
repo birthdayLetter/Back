303588aa188442a90c4b0f0c38c10db734ab4b35
seonkyoung <sunkyoung.dev@gmail.com>
2025-04-28 15:27:44 +0900

    Friend: 유저가 친구신청한 사람의 유저정보를 담고있는 user (db컬럼은 target_user_id)

--- src/main/java/com/springboot/letterbackend/data/entity/Friend.java
+++ src/main/java/com/springboot/letterbackend/data/entity/Friend.java
@@ -16,9 +16,12 @@ public class Friend {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(nullable = false)
-    private String userId;
-    @Column(nullable = false)
-    private String friendId;
+    private Long userId;
+
+    @ManyToOne(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
+    @JoinColumn(name = "target_user_id",referencedColumnName = "id")
+    private User user;
+
     @Column(nullable = false)
     private Status status;
 
