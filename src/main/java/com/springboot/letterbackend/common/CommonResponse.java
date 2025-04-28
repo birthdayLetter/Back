c32c500316ca9f701d107a4897f4d9da2a547e4c
seonkyoung <sunkyoung.dev@gmail.com>
2025-04-28 15:11:47 +0900

    CommonResponse: DUPLICATED(계정 중복표시) 변수 추가

--- src/main/java/com/springboot/letterbackend/common/CommonResponse.java
+++ src/main/java/com/springboot/letterbackend/common/CommonResponse.java
@@ -1,7 +1,7 @@
 package com.springboot.letterbackend.common;
 
 public enum CommonResponse {
-    SUCCESS(0,"Success"),FAIL(-1,"Fail"),PENDING(1,"Pending");
+    SUCCESS(0,"Success"),FAIL(-1,"Fail"),PENDING(1,"Pending"),DUPLICATED(2,"User is already exsited.");
     int code;
     String msg;
 
