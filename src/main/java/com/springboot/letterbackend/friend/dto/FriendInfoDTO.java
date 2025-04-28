0bfb64b669340999d46f5eb8696c6904beac8592
seonkyoung <sunkyoung.dev@gmail.com>
2025-04-28 15:12:25 +0900

    FriendInfoDTO: 친구유저 정보 가져오는 dto 추가

---
+++ src/main/java/com/springboot/letterbackend/friend/dto/FriendInfoDTO.java
@@ -0,0 +1,30 @@
+package com.springboot.letterbackend.friend.dto;
+
+import com.springboot.letterbackend.data.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+//친구 개인의 정보가 담긴 dto
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class FriendInfoDTO {
+    private Long id;
+    private String name;
+    private String uid;
+    private LocalDate birthDay;
+    private String description;
+
+    public FriendInfoDTO(User friendUser) {
+        this.id=friendUser.getId();
+        this.name=friendUser.getName();
+        this.uid=friendUser.getUid();
+        this.birthDay=friendUser.getBirthDay();
+        this.description=friendUser.getDesctiption();
+    }
+}
