97e346b8e3c206c483d3ee3111dd8df39e9cb7e1
seonkyoung <sunkyoung.dev@gmail.com>
2025-04-28 15:28:24 +0900

    CheckServiceImpl: logger 추가 CheckPassword 메서드 추가

--- src/main/java/com/springboot/letterbackend/user/service/impl/CheckServiceImpl.java
+++ src/main/java/com/springboot/letterbackend/user/service/impl/CheckServiceImpl.java
@@ -2,15 +2,21 @@ package com.springboot.letterbackend.user.service.impl;
 
 import com.springboot.letterbackend.data.entity.User;
 import com.springboot.letterbackend.data.repository.UserRepository;
+import com.springboot.letterbackend.user.controller.SiginController;
 import com.springboot.letterbackend.user.service.CheckService;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class CheckServiceImpl implements CheckService {
 
     final UserRepository userRepository;
     final PasswordEncoder passwordEncoder;
+    private final Logger logger = LoggerFactory.getLogger(CheckServiceImpl.class);
 
     public CheckServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
@@ -39,10 +45,9 @@ public class CheckServiceImpl implements CheckService {
 
     @Override
     public boolean CheckPassword(String enterPassword,User user) {
-        if(passwordEncoder.matches(enterPassword,user.getPassword())){
-            return true;
-        }
-        return false;
+        logger.info("비교작업을 수행합니다.");
+        logger.info(enterPassword);
+        return  passwordEncoder.matches(enterPassword,user.getPassword());
 
 
     }
