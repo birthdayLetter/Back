300f5e55f5608c6e88ecaccc1871b30076b680d8
seonkyoung <sunkyoung.dev@gmail.com>
2025-04-29 20:18:23 +0900

    SignUpResultDto: setSuccessResult,setFailResult 서비스에서 메서드 옮겨옴

--- src/main/java/com/springboot/letterbackend/user/dto/SignUpResultDto.java
+++ src/main/java/com/springboot/letterbackend/user/dto/SignUpResultDto.java
@@ -1,5 +1,6 @@
 package com.springboot.letterbackend.user.dto;
 
+import com.springboot.letterbackend.common.CommonResponse;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,4 +14,19 @@ public class SignUpResultDto {
     private boolean success;
     private int code;
     private String msg;
+
+
+
+    //DTO쪽 메서드로 분리하기
+    public void setSuccessResult(SignUpResultDto signUpResultDto) {
+        signUpResultDto.setSuccess(true);
+        signUpResultDto.setCode(CommonResponse.SUCCESS.getCode());
+        signUpResultDto.setMsg(CommonResponse.SUCCESS.getMsg());
+    }
+    public void setFailResult(SignUpResultDto signUpResultDto) {
+        signUpResultDto.setSuccess(false);
+        signUpResultDto.setCode(CommonResponse.FAIL.getCode());
+        signUpResultDto.setMsg(CommonResponse.FAIL.getMsg());
+
+    }
 }
