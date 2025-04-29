48a3b4cdc34214831ede4537360728161ff6c82f
seonkyoung <sunkyoung.dev@gmail.com>
2025-04-29 20:15:41 +0900

    Friend: 다대일관계 수정

--- src/main/java/com/springboot/letterbackend/data/entity/Friend.java
+++ src/main/java/com/springboot/letterbackend/data/entity/Friend.java
@@ -15,12 +15,16 @@ public class Friend {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(nullable = false)
-    private Long userId;
 
     @ManyToOne(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
-    @JoinColumn(name = "target_user_id",referencedColumnName = "id")
-    private User user;
+    @ToString.Exclude // 순환참조오류 방지
+    @JoinColumn(name = "from_user_id",referencedColumnName = "id")
+    private User fromUser; // 신청자 Id
+
+    @ManyToOne(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
+    @ToString.Exclude // 순환참조오류 방지
+    @JoinColumn(name = "to_user_id",referencedColumnName = "id")
+    private User toUser; //신청받은자 ID
 
     @Column(nullable = false)
     private Status status;
