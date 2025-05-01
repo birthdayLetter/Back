95be1f574a62cfb357e5235761c9462404acf178
seonkyoung <sunkyoung.dev@gmail.com>
2025-05-01 23:49:14 +0900

    SwaggerConfiguration: 경로 수정

--- src/main/java/com/springboot/letterbackend/config/SwaggerConfiguration.java
+++ src/main/java/com/springboot/letterbackend/config/SwaggerConfiguration.java
@@ -2,7 +2,13 @@ package com.springboot.letterbackend.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,6 +28,24 @@ public class SwaggerConfiguration {
         String[] paths = {
                 "/**",
         };
+        String securitySchemeName = "Authorization";
+
+
+//        OpenApiCustomizer openApiCustomizer=new OpenApiCustomizer() {
+//            @Override
+//            public void customise(OpenAPI openApi) {
+//                openApi.addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+//                        .components(new Components()
+//                                .addSecuritySchemes(securitySchemeName,new SecurityScheme()
+//                                        .name(securitySchemeName)
+//                                        .type(SecurityScheme.Type.HTTP)
+//                                        .scheme("bearer")
+//                                        .bearerFormat("JWT")
+//                                )
+//                        );
+//
+//            }
+//        };
         return GroupedOpenApi
                 .builder()
                 .group("letterbackend")
