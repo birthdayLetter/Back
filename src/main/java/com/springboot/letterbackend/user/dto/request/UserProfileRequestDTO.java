755a55ab7e06c5fad8b9492e2e32b2f09fc1e689
seonkyoung <sunkyoung.dev@gmail.com>
2025-05-02 01:02:57 +0900

    UserProfileRequestDTO: MultiPart 파일 받을 수 있게 변경

--- src/main/java/com/springboot/letterbackend/user/dto/request/UserProfileRequestDTO.java
+++ src/main/java/com/springboot/letterbackend/user/dto/request/UserProfileRequestDTO.java
@@ -1,6 +1,7 @@
 package com.springboot.letterbackend.user.dto.request;
 
 import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDate;
 
@@ -15,6 +16,6 @@ public class UserProfileRequestDTO {
     String email;
     LocalDate birthDay;
     String description;
-    String profileUrl;
+    MultipartFile profileImg;
     String password;
 }
