2aec69dc78c18ca719ffac1dd821997a967ed9b6
seonkyoung <sunkyoung.dev@gmail.com>
2025-05-02 01:23:25 +0900

    UserProfileService: String profileImgUrl 직접 주입 제거. dto로 들어감

--- src/main/java/com/springboot/letterbackend/user/service/UserProfileService.java
+++ src/main/java/com/springboot/letterbackend/user/service/UserProfileService.java
@@ -9,6 +9,6 @@ public interface UserProfileService {
     UserProfileResponseDTO getUserProfile(User user);
 
     // 유저 개인 정보를 수정하는 서비스
-    UserProfileResponseDTO editUserProfile(UserProfileRequestDTO userProfileReponseDTO, User user,String progfileurl);
+    UserProfileResponseDTO editUserProfile(UserProfileRequestDTO userProfileReponseDTO, User user);
     // 유저가 편지를 클릭 했을때 읽었다고 상태를 수정합니다.
 }
