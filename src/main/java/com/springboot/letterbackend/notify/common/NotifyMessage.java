901abe97058d6d39a633a7ba048d84c0bef25503
seonkyoung <sunkyoung.dev@gmail.com>
2025-05-10 14:21:20 +0900

    NotifyMessage

---
+++ src/main/java/com/springboot/letterbackend/notify/common/NotifyMessage.java
@@ -0,0 +1,15 @@
+package com.springboot.letterbackend.notify.common;
+
+public enum NotifyMessage {
+    NEW_FRIEND_REQUEST("새로운 친구 요청이 있습니다."),
+    NEW_FRIEND_RESPONSE("친구 수락이 되었습니다.");
+    private String message;
+
+    NotifyMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
