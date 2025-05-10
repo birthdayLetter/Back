953a8dffb972362f30033a8b7d7d2121d2d72df9
seonkyoung <sunkyoung.dev@gmail.com>
2025-05-10 14:20:14 +0900

    RequestLetterPostDto 생성

--- src/main/java/com/springboot/letterbackend/letter/dto/RequestLetterPostDTO.java
+++ src/main/java/com/springboot/letterbackend/letter/dto/RequestLetterPostDTO.java
@@ -2,14 +2,19 @@ package com.springboot.letterbackend.letter.dto;
 
 import lombok.*;
 
+import java.time.LocalDate;
+
 @Getter@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class RequestLetterPostDTO {
-    String token;
+    long letterId;
+    String fromUser;//받는사람
+    String toUser;//보내는 사람
     String content;
-    String reciverId;
-    String senderId;
+    LocalDate date;
+    String letterTemplateUrl;
+
 
 }
