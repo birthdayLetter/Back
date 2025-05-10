6514bdeb874e31274762a418b7375e78e1c6e818
seonkyoung <sunkyoung.dev@gmail.com>
2025-05-10 14:16:29 +0900

    LetterRepository 변경

--- src/main/java/com/springboot/letterbackend/data/repository/LetterRepository.java
+++
@@ -1,7 +0,0 @@
-package com.springboot.letterbackend.data.repository;
-
-import com.springboot.letterbackend.data.entity.Letter;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LetterRepository extends JpaRepository<Letter, Long> {
-}
