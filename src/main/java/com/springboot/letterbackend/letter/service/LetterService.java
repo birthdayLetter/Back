f2d5fb88126c17cc1b248d2659730cf1161c077a
seonkyoung <sunkyoung.dev@gmail.com>
2025-05-10 14:19:16 +0900

    LetterService

---
+++ src/main/java/com/springboot/letterbackend/letter/service/LetterService.java
@@ -0,0 +1,18 @@
+package com.springboot.letterbackend.letter.service;
+
+import com.springboot.letterbackend.data.entity.User;
+import com.springboot.letterbackend.letter.dto.RequestLetterPostDTO;
+import com.springboot.letterbackend.letter.dto.ResponseLetterPostDTO;
+
+import java.util.List;
+
+public interface LetterService {
+
+    //유저의 모든 편지를 검색한다
+    public List<ResponseLetterPostDTO> getAllLetter(long toUserId);
+    //유저의 특정년도 편지를 검색
+    public List<ResponseLetterPostDTO> getAllLetterByYear(int year, long toUserId);
+    public void sendLetter(User user,RequestLetterPostDTO responseLetterPostDTO);
+}
+
+
