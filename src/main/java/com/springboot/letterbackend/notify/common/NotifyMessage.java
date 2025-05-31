bf342a865836ce6e8d7fd4e21ae9b8b2c5504c3a
seonkyoung <sunkyoung.dev@gmail.com>
2025-05-31 20:42:03 +0900

    알람 Message

--- src/main/java/com/springboot/letterbackend/notify/common/NotifyMessage.java
+++ src/main/java/com/springboot/letterbackend/notify/common/NotifyMessage.java
@@ -2,7 +2,8 @@ package com.springboot.letterbackend.notify.common;
 
 public enum NotifyMessage {
     NEW_FRIEND_REQUEST("새로운 친구 요청이 있습니다."),
-    NEW_FRIEND_RESPONSE("친구 수락이 되었습니다.");
+    NEW_FRIEND_RESPONSE("친구 수락이 되었습니다."),
+    NEW_LETTER_REQUEST("새로운 편지가 도착했습니다");
     private String message;
 
     NotifyMessage(String message) {
