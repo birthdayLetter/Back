e7f2b13c7dda04fe7dd9a160bca4aa361fe1ea2c
seonkyoung <sunkyoung.dev@gmail.com>
2025-05-31 20:42:57 +0900

    NeedNotificationType Enum

--- src/main/java/com/springboot/letterbackend/notify/common/NotificationType.java
+++ src/main/java/com/springboot/letterbackend/notify/common/NotificationType.java
@@ -1,5 +1,8 @@
 package com.springboot.letterbackend.notify.common;
 
 public enum NotificationType {
-    YATA, REVIEW, CHAT
+    LETTER,FRIEND,NOTFICATION
+    //LETTER:편지 알림
+    //FRIEND:친구 신청,수랃 알림
+    //NOTIFICATION: 전체 공지
 }
