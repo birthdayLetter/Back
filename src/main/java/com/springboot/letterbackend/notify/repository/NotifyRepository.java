dd21e25bf4f16f7696921f97f761a0dbfde807c0
seonkyoung <sunkyoung.dev@gmail.com>
2025-05-31 20:41:51 +0900

    NofipyRepository

--- src/main/java/com/springboot/letterbackend/notify/repository/NotifyRepository.java
+++ src/main/java/com/springboot/letterbackend/notify/repository/NotifyRepository.java
@@ -2,6 +2,6 @@ package com.springboot.letterbackend.notify.repository;
 
 import com.springboot.letterbackend.notify.entity.Notify;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+// 알림객체를 저장하고 관리
 public interface NotifyRepository extends JpaRepository<Notify, Long> {
 }
