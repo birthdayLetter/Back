bba279ad7153de6be57b20d6116c7c85b846c2e3
seonkyoung <sunkyoung.dev@gmail.com>
2025-06-01 19:45:39 +0900

    dto에서 fromUser 삭제

--- src/main/java/com/springboot/letterbackend/letter/dto/RequestLetterPostDTO.java
+++ src/main/java/com/springboot/letterbackend/letter/dto/RequestLetterPostDTO.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @Builder
 public class RequestLetterPostDTO {
     //long letterId;이것도 필요없음.
-    String fromUser;//보내는 사람
+    //String fromUser;//보내는 사람
     String toUser;//받는사람
     String content;
     //LocalDateTime date; 이건 백엔드에서 처리하기
